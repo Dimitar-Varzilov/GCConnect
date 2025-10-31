709c4bb15568cb32fefe3fe3b20a55532b87baf8
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2025-10-31 19:15:01 +0000

    feat(router): add HomeComponent and set it as root route; move NavBar from AppComponent into Home

--- GCConnect.Client/src/app/app-routing.module.ts
+++ GCConnect.Client/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { ROUTES } from './common/constants/routes.constants';
 
 const routes: Routes = [
   {
     path: ROUTES.root,
-    component: AppComponent,
+    component: HomeComponent,
   },
   {
     path: ROUTES.loginCallback,
--- GCConnect.Client/src/app/app.module.ts
+++ GCConnect.Client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OktaAuthFactory } from './okta-auth.factory';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
